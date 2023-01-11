a002502bf1c92bab91b0f53f5a4c91c629c9e453
Deepak <trojanhorsedc@gmail.com>
2023-01-12 03:37:16 +0530

    Data Binding to Layout file (UI)

--- app/src/main/java/com/academy/mvvm/MainActivity.kt
+++ app/src/main/java/com/academy/mvvm/MainActivity.kt
@@ -2,10 +2,18 @@ package com.academy.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.academy.mvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding.quote = Quote("Do or Die","User")
+//        binding.tvQuote.text = "Do or Die"
+//        binding.tvAuthor.text="Anonymous"
     }
 }
---
+++ app/src/main/java/com/academy/mvvm/Quote.kt
@@ -0,0 +1,3 @@
+package com.academy.mvvm
+
+data class Quote(val text:String, val author:String)
